94f58420539cd16ce2139b86c6c0a0a99736baa8
Jonas Friedmann <j@frd.mn>
2014-11-19 16:12:32 +0100

    Commenting

--- parser.php
+++ parser.php
@@ -21,18 +21,22 @@ $time_start = microtime(true);
 function std($type, $msg) {
   global $log_debug, $log_info, $log_error;
 
+  // If logging "debug" is unwanted, leave function
   if (!$log_debug && $type == "debug") {
     return false;
   }
 
+  // If logging "info" is unwanted, leave function
   if (!$log_info && $type == "info") {
     return false;
   }
 
+  // If logging "error" is unwanted, leave function
   if (!$log_error && $type == "error") {
     return false;
   }
 
+  // Append log type as prefix
   if ($type == "error") {
     error_log("[".strtoupper($type)."] ".$msg);
   } else {
