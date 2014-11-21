686c673dd49111dd1c848bfbb183b7e040355296
Jonas Friedmann <j@frd.mn>
2014-11-21 10:01:53 +0100

    Add config file

---
+++ lib/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Configuration options
+ */
+
+$log_debug = false;
+$log_info = true;
+$log_error = true;
+
+$items_per_page = 100;
+
+$cache_file =  $_SERVER['HOME']."/.ppm.cache";
+
+?>
